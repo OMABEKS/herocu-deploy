63583c12043b37fe0dd9725b7eeebd46ae906780
Omurbek <92911400+OMABEKS@users.noreply.github.com>
2021-12-17 11:34:40 +0600

    api

--- src/helpers/consts.js
+++ src/helpers/consts.js
@@ -1,3 +1,3 @@
-export const PRODUCTS_API = "http://localhost:8000/products";
+export const PRODUCTS_API = "https://omabeksultanov98.herokuapp.com/apiproducts";
 
-export const BRANDS_API = "http://localhost:8000/brands";
+export const BRANDS_API = "https://omabeksultanov98.herokuapp.com/apibrands";
